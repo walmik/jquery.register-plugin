8ff302960b9dfe7d2789fce07d1c2686f1536c2c
Walmik <walmik.deshpande@gmail.com>
2014-08-03 10:27:21 -0700

    Added demonstration to call one plugin s func from another

--- a.js
+++ a.js
@@ -1,7 +1,30 @@
 var aObj = {
 
+	/**
+	 * The init function is called by default (if it exists)
+	 * You can remove it if you don't need it.
+	 */
 	init: function() {
-		console.log('a');
+
+		//We will need the value for `this` in the on click event handler
+		//to reference this object's properties
+		var self = this;
+
+		//Initialize action on click of button
+		this.$('.btn').on('click', function(){
+			self.methodA();
+		});
+
+	},
+
+
+	/**
+	 * Any functionality you need to add can be a propery in this object
+	 */
+	methodA: function() {
+
+		alert('Hello from A');
+
 	}
 
 };
--- b.js
+++ b.js
@@ -1,7 +1,31 @@
 var bObj = {
 
+	/**
+	 * The init function is called by default (if it exists)
+	 * You can remove it if you don't need it.
+	 */
 	init: function() {
-		console.log('b');
+
+		//We will need the value for `this` in the on click event handler
+		//to reference this object's properties
+		var self = this;
+
+		//Initialize action on click of button 1
+		this.$('.btn1').on('click', function(){
+			self.methodB();
+		});
+
+		//Initialize action on click of button 2
+		//and call a function from the other DIV's plugin
+		this.$('.btn2').on('click', function(){
+			$('.a').aPlug('methodA');
+		});
+
+	},
+
+
+	methodB: function() {
+		alert('Hello from B!');
 	}
 
 };
